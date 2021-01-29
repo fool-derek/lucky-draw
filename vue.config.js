6e4338e3415d2991aff36be702cda1871a65be14
Derek Chen <ccdaccd@163.com>
2021-01-29 10:29:36 +0800

    Using vue-cli-plugin-electron-builder to build electron app

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,20 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
   configureWebpack: {
     devtool: 'source-map'
+  },
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        appId: 'com.lucky-draw.app',
+        win: {
+          icon: './src/assets/icon.ico',
+          target: 'msi'
+        },
+        mac: {
+          icon: './src/assets/icon.icns',
+          category: 'public.app-category.games'
+        }
+      }
+    }
   }
 };
